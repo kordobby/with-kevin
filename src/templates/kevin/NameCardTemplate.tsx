f6f7b0fa8568b448ff0af48be38ec2a1cc997263
kevinkim910408 <junho2413@gmail.com>
2023-03-27 17:28:00 -0400

    remove

--- src/templates/kevin/NameCardTemplate.tsx
+++ src/templates/kevin/NameCardTemplate.tsx
@@ -1,15 +1,10 @@
-import Contents from "../../components/layout/kevin/Contents";
-import Layout from "../../components/layout/kevin/Layout";
-import Title from "../../components/layout/kevin/Title";
 import NameCard from "../../components/nameCard/kevin/NameCard";
 
 const NameCardTemplate = () => {
   return (
-    <Layout>
-      <Title>NameCard</Title>
-      <Title types="subtitle">NameCard</Title>
+    <>
       <NameCard />
-    </Layout>
+    </>
   );
 };
 
