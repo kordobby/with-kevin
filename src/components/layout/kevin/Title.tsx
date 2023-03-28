42ca556cc8fbec5a1a0a209ba0349e22597d8a6a
kevinkim910408 <junho2413@gmail.com>
2023-03-28 17:40:55 -0400

    title work

---
+++ src/components/layout/kevin/Title.tsx
@@ -0,0 +1,45 @@
+import styled, { css } from "styled-components";
+
+type TitleType = "main" | "sub" | "desc";
+
+interface TitleInterface {
+  children: string;
+  types: TitleType;
+}
+
+const Title = ({ children, types }: TitleInterface) => {
+  return (
+    <TitleWrapper types={types}>
+      <span>{children}</span>
+    </TitleWrapper>
+  );
+};
+
+const TitleWrapper = styled.div<{ types: TitleType }>`
+  color: white;
+  ${({ types }) => {
+    switch (types) {
+      case "main":
+        return css`
+          font-weight: 700;
+          font-size: 2rem;
+        `;
+      case "sub":
+        return css`
+          font-weight: 600;
+          font-size: 1.5rem;
+        `;
+      case "desc":
+        return css`
+          font-weight: 500;
+          font-size: 1.2rem;
+        `;
+      default:
+        return;
+    }
+  }}
+`;
+
+const TitleText = styled.span``;
+
+export default Title;
--- src/templates/kevin/NameCardTemplate.tsx
+++ src/templates/kevin/NameCardTemplate.tsx
@@ -1,9 +1,13 @@
 import Layout from "@/components/layout/kevin/Layout";
+import Title from "@/components/layout/kevin/Title";
 import NameCard from "@/components/nameCard/kevin/NameCard";
 
 const NameCardTemplate = () => {
   return (
     <Layout>
+      <Title types="main">Kevin Kim</Title>
+      <Title types="sub">Name Card Work</Title>
+      <Title types="desc">Date: blah blah</Title>
       <NameCard />
     </Layout>
   );
