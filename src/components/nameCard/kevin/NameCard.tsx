23305388cb7fac11bbd6a657dae95c90956620e9
kevinkim910408 <junho2413@gmail.com>
2023-03-28 17:58:55 -0400

    basic cart set

--- src/components/nameCard/kevin/NameCard.tsx
+++ src/components/nameCard/kevin/NameCard.tsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
 const NameCard = () => {
-  return <></>;
+  return <Card></Card>;
 };
 
 export default NameCard;
+
+const Card = styled.div`
+  width: 320px;
+  height: 490px;
+  background: #272727;
+  border: 1px solid white;
+  border-radius: 12px;
+`;
