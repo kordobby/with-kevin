3efa3ea13d1119cd1edd63fb0cf4fab6d805308f
kevinkim910408 <junho2413@gmail.com>
2023-03-30 17:55:37 -0400

    transform anim

--- src/components/nameCard/kevin/NameCardAnim.ts
+++ src/components/nameCard/kevin/NameCardAnim.ts
@@ -3,18 +3,22 @@ import { keyframes } from "styled-components";
 export const fadeIn = keyframes`
   from {
     opacity: 0;
+    transform: translateY(10%);
   }
   to {
     opacity: 1;
+    transform: translateY(0%);
   }
 `;
 
 export const fadeOut = keyframes`
   from {
     opacity: 1;
+    transform: translateY(0%);
   }
   to {
     opacity: 0;
+    transform: translateY(10%);
   }
 `;
 
