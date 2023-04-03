2541a3d619a7201a72e1f67464d7ec5dad1ac749
kevinkim910408 <junho2413@gmail.com>
2023-04-03 16:12:47 -0400

    storybook set up

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+export default {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxBabelOptions: {
+          remarkPlugins: [require("remark-gfm")],
+        },
+        sourceLoaderOptions: {
+          injectStoryParameters: false,
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+};
