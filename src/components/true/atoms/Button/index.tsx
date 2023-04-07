1a2037b6170232edccbdb364bdc569d308f5af76
Yoon Lee <poordobby@naver.com>
2023-04-07 21:43:50 +0900

    fix. Button Component basic form

--- src/components/true/atoms/Button/index.tsx
+++ src/components/true/atoms/Button/index.tsx
@@ -1,16 +1,35 @@
 import { FC, HTMLProps } from "react";
 import { ButtonLayout, ButtonLayoutProps } from "./Button.styles";
+import { createContext, useContext } from "react";
 
+interface ButtonContextProps {
+  size: "lg" | "sm" | "md";
+  icon: React.ReactNode;
+}
+const ButtonContext = createContext<ButtonContextProps>({
+  size: "lg",
+  icon: null,
+});
 export interface ButtonProps
   extends HTMLProps<HTMLButtonElement>,
     ButtonLayoutProps {}
 
-const ButtonText = () => {
-  return <></>;
+const Button: FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <ButtonContext.Provider value={{ size: "lg", icon: null }}>
+      <ButtonLayout buttonType="primary" onClick={onClick}>
+        <ButtonIconSlot />
+        <span>{children}</span>
+      </ButtonLayout>
+    </ButtonContext.Provider>
+  );
 };
 
-const Button: FC<ButtonProps> = ({ children }) => {
-  return <ButtonLayout buttonType="primary">{children}</ButtonLayout>;
+const ButtonIconSlot = () => {
+  const { icon } = useContext(ButtonContext);
+
+  if (!icon) return null;
+  return <>{icon}</>;
 };
 
 export default Button;
