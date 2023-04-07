041ca31d74ebda314298d0db0172eaa34f21a078
Yoon Lee <poordobby@naver.com>
2023-04-07 22:35:26 +0900

    styles. add Button styles

--- src/components/true/atoms/Button/Button.styles.ts
+++ src/components/true/atoms/Button/Button.styles.ts
@@ -1,28 +1,40 @@
 import styled, { css } from "styled-components";
+import { colors } from "@/utils/YoontilColors";
 
 export type ButtonTypes = "primary" | "ghost" | "default";
 
 export interface ButtonLayoutProps {
   buttonType: ButtonTypes;
 }
+const getButtonStyles = (buttonType: ButtonTypes, color: string) => {
+  const primaryButtonStyles = css`
+    background-color: ${color};
+    color: white;
+  `;
+  const ghostButtonStyles = css`
+    color: ${color};
+    border: 1px solid ${color};
+  `;
+  const defaultButtonStyles = css``;
 
-const primaryButtonStyles = css``;
-const ghostButtonStyles = css``;
-const defaultButtonStyles = css``;
+  const source = new Map([
+    ["primary", primaryButtonStyles],
+    ["ghost", ghostButtonStyles],
+    ["default", defaultButtonStyles],
+  ]);
+
+  return source.get(buttonType);
+};
 
 export const ButtonLayout = styled.button<ButtonLayoutProps>`
   padding: 10px 30px;
+  color: white;
   border: none;
   width: 100%;
+  border-radius: 5px;
   cursor: pointer;
-  ${({ buttonType }) => {
-    const source = new Map([
-      ["primary", primaryButtonStyles],
-      ["ghost", ghostButtonStyles],
-      ["default", defaultButtonStyles],
-    ]);
-
-    return source.get(buttonType);
+  ${({ buttonType, color }) => {
+    return getButtonStyles(buttonType, color ?? "black");
   }}
 `;
 
