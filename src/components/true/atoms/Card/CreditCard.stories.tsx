2e665b2494ab0a9093fca87f90118fa3426d0c7b
Yoon Lee <poordobby@naver.com>
2023-04-08 11:50:42 +0900

    add. card and loyout stories

---
+++ src/components/true/atoms/Card/CreditCard.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import CreditCard from "./CreditCard";
+
+//👇 This default export determines where your story goes in the story list
+const meta: Meta<typeof CreditCard> = {
+  title: "TrueComponents/Credit Card",
+  component: CreditCard,
+};
+
+export default meta;
+type Story = StoryObj<typeof CreditCard>;
+
+export const CreditCardStory: Story = {
+  args: {
+    //👇 The args you need here will depend on your component
+  },
+};
